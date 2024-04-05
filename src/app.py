304acc15bd0096b0c0aba0c69eb0a2f18030c1a6
billwan96 <billwan@student.ubc.ca>
2024-04-04 22:51:34 -0700

    This commit create the plots for fentanyl and demographic with limited interactivity

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 
@@ -13,6 +15,71 @@ df = pd.DataFrame({
 })
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# 3rd plot: create plot related to fentanyl
+df_rate = pd.read_csv('data/processed/od_deathrate.csv', index_col=0)
+df_rate = df_rate[:-2]
+df_plot3 = df_rate[['Synthetic Opioids other than Methadone (primarily fentanyl)3',
+                   'Synthetic Opioids other than Methadone (primarily fentanyl)3 Female',
+                   'Synthetic Opioids other than Methadone (primarily fentanyl)3 Male']]
+categories = df_plot3.index 
+fen_fig = go.Figure()
+
+@app.callback(
+    Output('fen-graph', 'figure'),  # id of your graph component
+    [Input('gender-dropdown', 'value')]
+)
+def update_figure(selected_gender):
+    # Filter your DataFrame based on the selected gender
+    if selected_gender == 'All':
+        df_filtered = df_plot3
+    elif selected_gender == 'Male':
+        df_filtered = df_plot3[['Synthetic Opioids other than Methadone (primarily fentanyl)3 Male']]
+    elif selected_gender == 'Female':
+        df_filtered = df_plot3[['Synthetic Opioids other than Methadone (primarily fentanyl)3 Female']]
+    else:
+        df_filtered = df_plot3
+
+    # Create a trace for each dataset
+    traces = []
+    for col in df_filtered.columns:
+        traces.append(go.Scatter(x=df_filtered.index, y=df_filtered[col], mode='lines', name=selected_gender))
+
+    # Create the data and layout objects
+    layout = go.Layout(title=f'Death Rate Related to Fentanyl for {selected_gender}',
+                       yaxis=dict(title='Death Rate'),
+                       xaxis=dict(title='Year'),
+                       legend=dict(yanchor="top", y=-0.4, 
+                                   xanchor="center", x=0.5,
+                                   font=dict(size=8))) 
+
+    # Create the figure and add the data and layout
+    fen_fig = go.Figure(data=traces, layout=layout)
+
+    return fen_fig
+
+
+# 4th plot: create plot for the demographic
+df_demo = pd.read_csv('data/processed/od_rate_demo.csv', index_col=0)
+df_demo = df_demo[:-1]
+categories = df_demo.index
+trace1 = go.Bar(x=categories, y=df_demo['total White (Non-Hispanic)'], name='Total White')
+trace2 = go.Bar(x=categories, y=df_demo['total Black (Non-Hispanic)'], name='Total Black')
+trace3 = go.Bar(x=categories, y=df_demo['total Asian* (Non-Hispanic)'], name='Total Asian')
+trace4 = go.Bar(x=categories, y=df_demo['total Native Hawaiin or Other Pacific Islander* (Non-Hispanic)'], name='Total Native Hawaiin or Other Pacific Islander')
+trace5 = go.Bar(x=categories, y=df_demo['total Hispanic'], name='Total Hispanic')
+trace6 = go.Bar(x=categories, y=df_demo['total American Indian or Alaska Native (Non-Hispanic)'], name='Total American Indian or Alaska Native')
+data = [trace1, trace2, trace3, trace4, trace5, trace6]
+layout = go.Layout(title= "Overdose Death Rate based on Demographic",
+                   yaxis=dict(title='Death Rate'),
+                   xaxis= dict(title= 'Year'),
+                   legend=dict(yanchor="top", y=-0.4, 
+                               xanchor="center", x=0.5,
+                               font=dict(size=8)),
+                   barmode='group')
+demo_fig = go.Figure(data=data, layout=layout)
+
+
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
@@ -58,12 +125,26 @@ sidebar = html.Div(
 )
 
 
-main_dashboard = html.Div(children=[
-    html.Div(children='Overdose deaths'),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig),
+main_dashboard = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.Div(children='Overdose deaths')),
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='example-graph', figure=fig)),
+    ]),
+    dbc.Row([
+        #testing of filter gender to be removed later
+        dbc.Col(dcc.Dropdown(
+        id='gender-dropdown',
+        options=[
+            {'label': 'All Genders', 'value': 'All'},
+            {'label': 'Male', 'value': 'Male'},
+            {'label': 'Female', 'value': 'Female'}
+        ],
+        value='All')),
+        dbc.Col(dcc.Graph(id='fen-graph', figure=fen_fig)),
+        dbc.Col(dcc.Graph(id='demo-graph', figure=demo_fig))
+    ])
 ],
     id="page-content",
     style=CONTENT_STYLE)
