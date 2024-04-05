68963f0afeae8b643bbefbfc614955a916749420
SoloSynth1 <solosynth1@gmail.com>
2024-04-05 01:59:33 -0700

    wip: re-do data preprocessing script to make each column a feature

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,149 +1,95 @@
-"""
-This script processes overdose data from an Excel file. It includes functions to:
-
-1. Read and preprocess the data.
-2. Rename columns based on gender and drug type.
-3. Append a suffix to column names.
-
-The script then uses these functions to process data in every sheet, rename the gender columns, and export the processed data to a CSV file.
-"""
-
+import os
 import pandas as pd
+from parse_config import config, overall_columns, specific_columns, demographic_columns
 
-def process_overdose_data(sheet_name):
+
+RAW_DATA_FILE = "./data/raw/Overdose_data_1999-2021 1.19.23.xlsx"
+OUTPUT_DIR = "./data/processed/"
+
+
+def read_raw_data():
+    """Reads in raw data.
+
+    Returns
+    -------
+    dict
+        A dictionary that contain pandas data frames.
+        The data frames contain the raw data with non-relevant rows and columns dropped.
+
     """
-    This function reads an Excel file of overdose data, processes it, and returns a transposed DataFrame.
-    
-    Parameters:
-    sheet_name (str): The name of the sheet in the Excel file to read.
-    
-    Returns:
-    df_processed (DataFrame): The processed DataFrame.
+    raw_dfs = {}
+    for k, v in config.items():
+        raw_dfs[k] = pd.read_excel(RAW_DATA_FILE, sheet_name=k, skiprows=6)
+        raw_dfs[k] = raw_dfs[k].drop(columns=['Unnamed: 0'], axis=1)
+        raw_dfs[k].set_index('Unnamed: 1', inplace=True)
+        raw_dfs[k] = raw_dfs[k].iloc[0:v['valid_rows']].loc[:, 1999:2021]
+    return raw_dfs
+
+
+def get_deaths_and_rate(raw_dfs, num_df_name, rate_df_name, pop_type):
+    """Parse and convert the data into a long format.
+
+    Parameters
+    ----------
+    raw_dfs
+        Dictionary returned from read_raw_data().
+
+    num_df_name
+        Name of the Excel Spreadsheet that contains the number of deaths.
+
+    rate_df_name
+        Name of the Excel Spreadsheet that contains the death rates.
+
+    pop_type
+        Name of the population (i.e. "Overall"/"Young Adult")
+
+    Returns
+    -------
+    (list, list)
+        Two lists containing deaths, death rates and other relevant attributes.
+        The first list contains the overall figures for each year, while the second list
+        contains figures from specific drug types and other relevant attributes.
+
     """
-    # Read the Excel file
-    df_load = pd.read_excel('../data/raw/Overdose_data_1999-2021 1.19.23.xlsx', sheet_name=sheet_name, skiprows=6)
-    # Drop the first column
-    df_load = df_load.drop(df_load.columns[0], axis=1)
-    # Set the index using the first column
-    df_load.set_index(df_load.columns[0], inplace=True)
-    # Transpose the DataFrame
-    df_processed = df_load.T
-    # Remove leading spaces from column names
-    df_processed.columns = df_processed.columns.str.strip()
-    # Drop columns where all values are NaN
-    df_processed = df_processed.dropna(axis=1, how='all')
-    
-    return df_processed
+    overall_rows = []
+    specific_rows = []
 
-def rename_columns(df):
+    for year in range(1999, 2022):
+        for attributes, idx in config[num_df_name]['indices'].items():
+            deaths = raw_dfs[num_df_name].loc[:, year].iloc[idx]
+            rate = raw_dfs[rate_df_name].loc[:, year].iloc[idx]
+            if len(attributes) == 2:
+                overall_rows.append([*attributes, deaths, rate, year, pop_type])
+            else:
+                specific_rows.append([*attributes, deaths, rate, year, pop_type])
+
+    return overall_rows, specific_rows
+
+
+def preprocess():
+    """Preprocess the raw data.
     """
-    This function renames the columns of a DataFrame by prepending the drug type to 'Female' and 'Male'.
-    
-    Parameters:
-    df (DataFrame): The DataFrame to rename the columns of.
-    
-    Returns:
-    df (DataFrame): The DataFrame with renamed columns.
-    """
-    new_columns = []
-    for i in range(len(df.columns)):
-        if df.columns[i] == 'Female':
-            drug_type = df.columns[i - 1]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'Male':
-            drug_type = df.columns[i - 2]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        else:
-            new_columns.append(df.columns[i])
-    df.columns = new_columns
-    return df
+    raw_dfs = read_raw_data()
 
-def append_suffix(df, suffix):
-    """
-    This function appends a suffix to each column name in a DataFrame.
-    
-    Parameters:
-    df (DataFrame): The DataFrame to append the suffix to the columns of.
-    suffix (str): The suffix to append to each column name.
-    
-    Returns:
-    df (DataFrame): The DataFrame with the modified column names.
-    """
-    df.columns = [f"{col}_{suffix}" for col in df.columns]
-    return df
+    # handle overall data
+    o1, s1 = get_deaths_and_rate(raw_dfs,'Number Drug OD Deaths',
+                                 'Rate Drug OD Deaths',"Overall")
 
-# process overall no. of death
-sheet_name = 'Number Drug OD Deaths'
-df_od_death = process_overdose_data(sheet_name)
-df_od_death = rename_columns(df_od_death)
+    # handle young adult data
+    o2, s2 = get_deaths_and_rate(raw_dfs, 'Number Drug OD, 15-24 Years',
+                                 'Rate Drug OD, 15-24 Years', "Young Adults, 15-24 Years")
 
-# process no. of death for 15-24 yo
-sheet_name = 'Number Drug OD, 15-24 Years'
-df_od_death_youth = process_overdose_data(sheet_name)
-df_od_death_youth = rename_columns( df_od_death_youth)
-df_od_death_youth = append_suffix(df_od_death_youth, 'youth')
+    overall_rows = o1 + o2
+    specific_rows = s1 + s2
 
-# combine the no. of death
-df_od_death_all = pd.concat([df_od_death, df_od_death_youth], axis=1)
+    # TODO: handle demographic data
 
-# process overall rate of OD death
-sheet_name = 'Rate Drug OD Deaths'
-df_od_rate = process_overdose_data(sheet_name)
-df_od_rate = rename_columns(df_od_rate)
+    overall_df = pd.DataFrame(overall_rows, columns=overall_columns)
+    specific_df = pd.DataFrame(specific_rows, columns=specific_columns)
 
-# process rate of death for 15-24 yo
-sheet_name = 'Rate Drug OD, 15-24 Years'
-df_od_rate_youth = process_overdose_data(sheet_name)
-df_od_rate_youth = rename_columns( df_od_rate_youth)
-df_od_rate_youth = append_suffix(df_od_rate_youth, 'youth')
+    overall_df.to_csv(os.path.join(OUTPUT_DIR, "overall.csv"))
+    specific_df.to_csv(os.path.join(OUTPUT_DIR, "specific.csv"))
 
-# combine the rate of death
-df_od_rate_all = pd.concat([df_od_rate, df_od_rate_youth ], axis=1)
 
-# process demographic rate
-sheet_name = 'Rate OD by Demographic'
-df_rate_demo = process_overdose_data(sheet_name)
-df_rate_demo = rename_columns(df_rate_demo)
-df_rate_demo.columns.values[3:21] = ['total ' + col for col in df_rate_demo.columns[3:21]]
-
-def rename_race_columns(df):
-    """
-    This function renames the race columns of a DataFrame by prepending the drug type to each race category.
-    
-    Parameters:
-    df (DataFrame): The DataFrame to rename the columns of.
-    
-    Returns:
-    df (DataFrame): The DataFrame with renamed columns.
-    """
-    new_columns = []
-    for i in range(len(df.columns)):
-        if df.columns[i] == 'White (Non-Hispanic)':
-            drug_type = df.columns[i - 3]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'Black (Non-Hispanic)':
-            drug_type = df.columns[i - 4]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'Asian* (Non-Hispanic)':
-            drug_type = df.columns[i - 5]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'Native Hawaiian or Other Pacific Islander* (Non-Hispanic)':
-            drug_type = df.columns[i - 6]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'Hispanic':
-            drug_type = df.columns[i - 7]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        elif df.columns[i] == 'American Indian or Alaska Native (Non-Hispanic)':
-            drug_type = df.columns[i - 8]
-            new_columns.append(f"{drug_type} {df.columns[i]}")
-        else:
-            new_columns.append(df.columns[i])
-    df.columns = new_columns
-    return df
-
-df_rate_demo = rename_race_columns(df_rate_demo)
-
-# export to csv files
-df_od_death_all.to_csv('../data/processed/od_deathnumber.csv')
-df_od_rate_all.to_csv('../data/processed/od_deathrate.csv')
-df_rate_demo.to_csv('../data/processed/od_rate_demo.csv')
+if __name__ == "__main__":
+    preprocess()
