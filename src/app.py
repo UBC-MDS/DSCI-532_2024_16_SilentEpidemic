fdc2c7e6e96b4358b4673cdb7baa99a98f713c72
SoloSynth1 <solosynth1@gmail.com>
2024-04-05 22:05:54 -0700

    add footer at bottom of the sidebar - add short desc - add author names - add link to repo - add last updated date

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+from modules.components import footer
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -53,20 +54,17 @@ sidebar = html.Div(
         html.Div(children=[
             # TODO: Toggle button
         ]),
+        footer
     ],
     style=SIDEBAR_STYLE,
 )
 
 
 main_dashboard = html.Div(children=[
-    html.Div(children='Overdose deaths'),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig),
-],
-    id="page-content",
-    style=CONTENT_STYLE)
+        html.Div(children='Overdose deaths'),
+        dcc.Graph(id='example-graph', figure=fig),
+    ],
+    id="page-content", style=CONTENT_STYLE)
 
 
 app.layout = html.Div(children=[
