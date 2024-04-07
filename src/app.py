4f49861aa2fd77924e33bcc30f892d5ecc15f375
Orix Au Yeung <solosynth1@gmail.com>
2024-04-07 02:10:55 -0700

    Implement the card for the main chart the overdose deaths (#67)
    * wip: add datasets to be loaded
    
    * refactor
    - make use of data frames from `modules.datasets` instead of individual loadings
    - add CSS formating to make the board prettier
    
    * implement bubble chart for overdose deaths and death rates
    - add `fig_deaths_and_rates`
    - update CSS styles
    - change all "gender" references to "sex"
    
    * change instruct to run dashboard to use `gunicorn`

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
-from dash import Dash, html, dcc, callback, Output, Input
+import pandas as pd
+from dash import Dash, html, dcc, Output, Input
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
-import pandas as pd
-from modules.components import footer, footnote, death_card, death_rate_card, percentage_card, fold_change_card
+from .modules.datasets import specific_df, demo_df
+from .modules.components import footer, footnote, death_card, death_rate_card, percentage_card, fold_change_card
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, 'src/assets/styles.css'])
 server = app.server
@@ -11,11 +12,11 @@ server = app.server
 
 sidebar = html.Div(
     [
-        html.H1("National Overdose Deaths Tracker", className="display-4"),
+        html.H2("National Overdose Deaths Tracker", className="title"),
         html.Hr(),
         html.Div(children=[
-            html.H2("Filter by Gender"),
-            dcc.Dropdown(id='gender_dropdown', 
+            html.H4("Sex"),
+            dcc.Dropdown(id='sex_dropdown',
                          options=[
                              {'label': 'All Sexes', 'value': 'All'},
                              {'label': 'Male', 'value': 'Male'},
@@ -24,7 +25,7 @@ sidebar = html.Div(
                              value='All')
                              ]),
         html.Div(children=[
-            html.H2("Filter by Drug Type"),
+            html.H4("Drug Type"),
             dcc.Checklist(
                 id='drug_type_list',
                 options=[
@@ -42,7 +43,7 @@ sidebar = html.Div(
                            'Stimulants', 'Cocaine', 'Psychostimulants', 'Benzodiazepines', 'Antidepressants'])
                            ]),
         html.Div(children=[
-            html.H2("Filter by Year Range", style={'margin-bottom': '25px'}),
+            html.H4("Year Range", style={'margin-bottom': '25px'}),
             dcc.RangeSlider(
                 id='year_range_slider',
                 min=1999, max=2021, step=1,
@@ -55,7 +56,7 @@ sidebar = html.Div(
                 ),
                 ]),    
         html.Div(children=[
-            html.H2("Filter by Age Group"),
+            html.H4("Age Group"),
             dcc.RadioItems(
                 id='age_group_radio',
                 options=[
@@ -71,7 +72,6 @@ sidebar = html.Div(
 
 
 # create graph for the demographic
-df_demo = pd.read_csv('data/processed/demo.csv')
 fig_demo = go.Figure()
 @app.callback(
     [Output('demo_graph', 'figure'),
@@ -79,25 +79,23 @@ fig_demo = go.Figure()
     [Input('drug_type_list', 'value'),
      Input('year_range_slider', 'value')]
 )
-def update_figure(selected_drug, selected_years):
+def update_demo_figure(selected_drug, selected_years):
     if len(selected_drug) == 9:
         selected_drug = ['Total Overdose Deaths']
         title = "All Drugs"
     else:
         selected_drug = selected_drug
         title = f"For {' and '.join(selected_drug)}"
-    filtered_df = df_demo[(df_demo['Drug Type'].isin(selected_drug)) &
-                          (df_demo['Year'] >= selected_years[0]) &
-                          (df_demo['Year'] <= selected_years[1])]
+    filtered_df = demo_df[(demo_df['Drug Type'].isin(selected_drug)) &
+                          (demo_df['Year'] >= selected_years[0]) &
+                          (demo_df['Year'] <= selected_years[1])]
     fig_demo = px.bar(filtered_df, x="Year", y="Death Rate", color="Demographic", barmode="group")
     
     fig_demo.update_layout(
         xaxis_title="Year",
         yaxis_title="Death Rate <br>(per 100,000 population)",
         legend=dict(
-            x=0, y=-0.2,
-            xanchor='center', yanchor='top',
-            orientation='h',
+            orientation="h", yanchor="bottom", y=1.02,
             font=dict(size=8)))
 
     return fig_demo, title
@@ -107,9 +105,8 @@ demo_card = dbc.Card([
     html.H4("Overdose Death Rate based on Demographic", id="demo_title"),
     html.H6("Subtitle", id="demo_subtitle"),
     dcc.Graph(id='demo_graph', figure=fig_demo)
-])
+], body=True)
 
-opioid_data = pd.read_csv('data/processed/specific.csv')
 
 # Create Percentage of Overdoses Involving Opioids per Drug Type Chart
 # Create callback for the Percentage of Overdoses Involving Opioids per Drug Type Chart
@@ -118,16 +115,16 @@ fig_percent_opioid_deaths = go.Figure()
     [Output('percent_opioids', 'figure'),
      Output('opioid_subtitle', 'children')],
     [Input('drug_type_list', 'value'),
-     Input('gender_dropdown', 'value'),
+     Input('sex_dropdown', 'value'),
      Input('year_range_slider', 'value'),
      Input('age_group_radio', 'value')]
      )
-def update_opioid_figure(selected_drug, selected_gender, selected_years, selected_age):
+def update_opioid_figure(selected_drug, selected_sex, selected_years, selected_age):
     start_year = pd.to_datetime(selected_years[0], format='%Y')
     end_year = pd.to_datetime(selected_years[1], format='%Y')
 
     # Import dataset and data wrangling for Percentage of Overdoses Involving Opioids per Drug Type plot
-    opioid_data_mod = opioid_data.copy()
+    opioid_data_mod = specific_df.copy()
     opioid_data_mod = opioid_data_mod[(opioid_data_mod['Opioid Type'] == 'overall') | (opioid_data_mod['Opioid Type'] == 'any')]
     overall_deaths = opioid_data_mod[opioid_data_mod['Opioid Type'] == 'overall'].groupby(['Drug Type', 'Sex', 'Year', 'Population Type'])['Deaths'].sum()
     opioid_data_mod['Percent Opioid Deaths'] = opioid_data_mod.apply(lambda row: (row['Deaths'] / overall_deaths[(row['Drug Type'], row['Sex'], row['Year'], row['Population Type'])]) * 100 if (row['Drug Type'], row['Sex'], row['Year'], row['Population Type']) in overall_deaths.index else 0, axis=1)
@@ -141,27 +138,27 @@ def update_opioid_figure(selected_drug, selected_gender, selected_years, selecte
     else:
         age_display = selected_age
 
-    if selected_gender == 'All':
-        gender_display = "Both Sexes"
-        gender_categories = ['Male', 'Female']
+    if selected_sex == 'All':
+        sex_display = "Both Sexes"
+        sex_categories = ['Male', 'Female']
     else:
-        gender_display = selected_gender
-        gender_categories = [selected_gender]
+        sex_display = selected_sex
+        sex_categories = [selected_sex]
 
     drugs = set(selected_drug.copy())
     drugs_display = set(selected_drug.copy())
     if 'Any opioid' in drugs:
         drugs = list((drugs - {'Any opioid'}) | {'Prescription opioids', 'Synthetic opioids', 'Heroin'})
-        drugs_display = list((drugs_display - {'Prescription opioids', 'Synthetic opioids', 'Heroin'} ) | {'Any opioid'})
+        drugs_display = list((drugs_display - {'Prescription opioids', 'Synthetic opioids', 'Heroin'}) | {'Any opioid'})
 
     if set(drugs_display) == {'Any opioid', 'Stimulants', 'Cocaine', 'Psychostimulants', 'Benzodiazepines', 'Antidepressants'}:
-        title = f"All drugs and {gender_display} and {age_display}"
+        title = f"All drugs and {sex_display} and {age_display}"
     else:
-        title = f"For {' and '.join(drugs_display)} and {gender_display} and {age_display}"
+        title = f"For {' and '.join(drugs_display)} and {sex_display} and {age_display}"
 
     filtered_opioid_df = filtered_opioid_df[(filtered_opioid_df['Drug Type'].isin(drugs)) &
                                             filtered_opioid_df['Year'].between(start_year, end_year, inclusive='both') &
-                                            filtered_opioid_df['Sex'].isin(gender_categories) &
+                                            filtered_opioid_df['Sex'].isin(sex_categories) &
                                             (filtered_opioid_df['Population Type'] == selected_age)]
     filtered_opioid_df = filtered_opioid_df.groupby(['Drug Type', 'Year', 'Population Type', 'Sex'])['Percent Opioid Deaths'].sum().reset_index()
 
@@ -173,6 +170,7 @@ def update_opioid_figure(selected_drug, selected_gender, selected_years, selecte
     fig_percent_opioid_deaths.update_layout(
         xaxis_title='Year',
         yaxis_title='Overdoses Involving Opioids (%)',
+        legend=dict(orientation="h",yanchor="bottom", y=1.02)
     )
     fig_percent_opioid_deaths.update_yaxes(range=[0, 100])
 
@@ -183,7 +181,73 @@ opioid_card = dbc.Card([
     html.H4("Percentage of Overdoses Involving Opioids by Drug Type", id="opioid_title"),
     html.H6("Subtitle", id="opioid_subtitle"),
     dcc.Graph(id='percent_opioids', figure=fig_percent_opioid_deaths)
-])
+], body=True)
+
+
+
+fig_deaths_and_rates = go.Figure()
+
+@app.callback(
+    [Output('main_graph', 'figure'),
+     Output('main_subtitle', 'children')],
+    [Input('drug_type_list', 'value'),
+     Input('sex_dropdown', 'value'),
+     Input('year_range_slider', 'value'),
+     Input('age_group_radio', 'value')]
+)
+def update_main_figure(selected_drug, selected_sex, selected_years, selected_age):
+    main_df = specific_df[specific_df['Opioid Type'] == 'overall']
+    main_df['Year'] = pd.to_datetime(main_df['Year'], format='%Y')
+
+    start_year = pd.to_datetime(selected_years[0], format='%Y')
+    end_year = pd.to_datetime(selected_years[1], format='%Y')
+
+    # Cases to filter the dataset based on inputs
+    if selected_age == 'Overall':
+        age_display = "All Age Groups"
+    else:
+        age_display = selected_age
+
+    if selected_sex == 'All':
+        sex_display = "Both Sexes"
+        sex_categories = ['Male', 'Female']
+    else:
+        sex_display = selected_sex
+        sex_categories = [selected_sex]
+
+    drugs = set(selected_drug.copy())
+    drugs_display = set(selected_drug.copy())
+    if 'Any opioid' in drugs:
+        drugs = list((drugs - {'Any opioid'}) | {'Prescription opioids', 'Synthetic opioids', 'Heroin'})
+        drugs_display = list((drugs_display - {'Prescription opioids', 'Synthetic opioids', 'Heroin'}) | {'Any opioid'})
+
+    if set(drugs_display) == {'Any opioid', 'Stimulants', 'Cocaine', 'Psychostimulants', 'Benzodiazepines',
+                              'Antidepressants'}:
+        title = f"All drugs and {sex_display} and {age_display}"
+    else:
+        title = f"For {' and '.join(drugs_display)} and {sex_display} and {age_display}"
+
+    main_df = main_df[(main_df['Drug Type'].isin(drugs)) &
+                                            main_df['Year'].between(start_year, end_year, inclusive='both') &
+                                            main_df['Sex'].isin(sex_categories) &
+                                            (main_df['Population Type'] == selected_age)]
+    filtered_df = main_df.groupby(['Drug Type', 'Year', 'Population Type']).agg({'Deaths': 'sum', 'Death Rate': 'mean'}).reset_index()
+    filtered_df = filtered_df.fillna(0)
+
+    fig_deaths_and_rates = px.scatter(filtered_df, x='Year', y='Deaths', color='Drug Type', size='Death Rate', size_max=60)
+
+    # Update layout
+    fig_deaths_and_rates.update_layout(xaxis_title='Year', yaxis_title='Deaths',
+                                       legend=dict(orientation="h",yanchor="bottom", y=1.02))
+
+    return fig_deaths_and_rates.to_dict(), title
+
+
+main_card = dbc.Card([
+    html.H4("Overdoses Deaths and Death Rates by Drug Type", id="main_title"),
+    html.H6("Subtitle", id="main_subtitle"),
+    dcc.Graph(id='main_graph', figure=fig_deaths_and_rates)
+], body=True)
 
 
 main_dashboard = dbc.Container([
@@ -194,7 +258,7 @@ main_dashboard = dbc.Container([
         dbc.Col(fold_change_card, md=3),
     ]),
      dbc.Row([
-        dbc.Col(demo_card, md=12),
+        dbc.Col(main_card, md=12),
     ]),
     dbc.Row([
         dbc.Col(opioid_card, md=6),
