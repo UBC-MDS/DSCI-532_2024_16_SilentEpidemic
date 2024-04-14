88d88caa943c2819400c98e0403e741cde4c79c9
SoloSynth1 <solosynth1@gmail.com>
2024-04-14 02:03:42 -0700

    minor formatting

--- src/modules/components/footer.py
+++ src/modules/components/footer.py
@@ -4,7 +4,7 @@ from ..utils import REPO_URL, get_repo_last_updated_time
 
 
 footer = html.Footer(children=[
-    html.Div("This is dashboard for visualizing the trend in deaths caused by drug overdoses in the United States from 1999 to 2021."),
+    html.Div("This is a dashboard for visualizing the trend in deaths caused by drug overdoses in the United States from 1999 to 2021."),
     html.Br(),
     html.Div("Authors: Orix Au Yeung (@SoloSynth1), Yingzi Jin (@jinyz8888), Alysen Townsley (@AlysenTownsley), Bill Wan (@billwan96)"),
     html.Br(),
