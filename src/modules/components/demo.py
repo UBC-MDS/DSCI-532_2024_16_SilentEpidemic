425279b5f7ff115190d949e5969284097c8d70c9
billwan96 <billwan@student.ubc.ca>
2024-04-17 11:20:20 -0700

    solve the comments related to the filter code

--- src/modules/components/demo.py
+++ src/modules/components/demo.py
@@ -2,6 +2,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import Output, Input, html, dcc, callback
 import dash_bootstrap_components as dbc
+import pandas as pd
 
 from ..datasets import demo_df
 
@@ -17,16 +18,14 @@ fig_demo = go.Figure()
      Input('year_range_slider', 'value')]
 )
 def update_demo_figure(selected_drug, selected_years):
+    start_year, end_year = selected_years
+    filtered_df = demo_df[(demo_df['Year'].between(start_year, end_year))]
     if len(selected_drug) == 8:
         title = "All Drugs"
-        filtered_df = demo_df[(demo_df['Drug Type'] == 'Total Overdose Deaths') &
-                              (demo_df['Year'] >= selected_years[0]) &
-                              (demo_df['Year'] <= selected_years[1])]
+        filtered_df = filtered_df[(filtered_df['Drug Type'] == 'Total Overdose Deaths')]
     elif len(selected_drug) > 0:
         title = f"For {' and '.join(selected_drug)}"
-        filtered_df = demo_df[(demo_df['Drug Type'].isin(selected_drug)) &
-                              (demo_df['Year'] >= selected_years[0]) &
-                              (demo_df['Year'] <= selected_years[1])]
+        filtered_df = filtered_df[(filtered_df['Drug Type'].isin(selected_drug))]
         filtered_df = filtered_df.groupby(['Year', 'Demographic'])['Death Rate'].sum().reset_index()
     else:
         return go.Figure(), "Please select a drug type"
@@ -37,7 +36,7 @@ def update_demo_figure(selected_drug, selected_years):
 
     for trace in fig_demo.data:
         if trace.name == 'Overall':
-            trace.line = dict(width=2.5) 
+            trace.line = dict(width=5) 
         else:
             trace.opacity = 0.7
 
