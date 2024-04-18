218b24691e87132a3a828d09231bc962bb17ac0d
Bill Wan <143459323+billwan96@users.noreply.github.com>
2024-04-18 11:22:31 -0700

    feat: Change the demo plot to a line plot (#136)
    * change the demo plot to a line plot
    
    * solve the comments related to the filter code
    
    * fix legends and make them shorter
    
    * remove unused `pandas` import
    
    * reformat `demo.py`
    
    ---------
    
    Co-authored-by: SoloSynth1 <solosynth1@gmail.com>

--- src/modules/components/demo.py
+++ src/modules/components/demo.py
@@ -5,41 +5,46 @@ import dash_bootstrap_components as dbc
 
 from ..datasets import demo_df
 
-
 # create graph for the demographic
 fig_demo = go.Figure()
 
 
-@callback(
-    [Output('demo_graph', 'figure'),
-     Output('demo_subtitle', 'children')],
-    [Input('drug_type_list', 'value'),
-     Input('year_range_slider', 'value')]
-)
+@callback([
+    Output('demo_graph', 'figure'),
+    Output('demo_subtitle', 'children')
+], [
+    Input('drug_type_list', 'value'),
+    Input('year_range_slider', 'value')
+])
 def update_demo_figure(selected_drug, selected_years):
+    start_year, end_year = selected_years
+    filtered_df = demo_df[(demo_df['Year'].between(start_year, end_year))]
     if len(selected_drug) == 8:
         title = "All Drugs"
-    else:
+        filtered_df = filtered_df[(filtered_df['Drug Type'] == 'Total Overdose Deaths')]
+    elif len(selected_drug) > 0:
         title = f"For {' and '.join(selected_drug)}"
-    filtered_df = demo_df[(demo_df['Drug Type'].isin(selected_drug)) &
-                          (demo_df['Year'] >= selected_years[0]) &
-                          (demo_df['Year'] <= selected_years[1])]
-    fig_demo = px.bar(
-        filtered_df, x="Year", y="Death Rate", color="Demographic",
-        barmode="group", color_discrete_sequence=px.colors.qualitative.T10)
+        filtered_df = filtered_df[(filtered_df['Drug Type'].isin(selected_drug))]
+        filtered_df = filtered_df.groupby(['Year', 'Demographic'])['Death Rate'].sum().reset_index()
+    else:
+        return go.Figure(), "Please select a drug type"
 
-    fig_demo.update_layout(
-        xaxis_title="Year",
-        yaxis_title="Death Rate <br>(per 100,000 population)",
-        legend=dict(
-            orientation="h", yanchor="bottom", y=1.02,
-            font=dict(size=8)))
+    fig_demo = px.line(filtered_df, x="Year", y="Death Rate", color="Demographic", line_group="Demographic",
+        color_discrete_sequence=px.colors.qualitative.T10)
+
+    for trace in fig_demo.data:
+        if trace.name == 'Overall':
+            trace.line = dict(width=5)
+        else:
+            trace.opacity = 0.7
+        trace.name = trace.name.replace(" (Non-Hispanic)", "")
+
+    fig_demo.update_layout(xaxis_title="Year", yaxis_title="Death Rate <br>(per 100,000 population)",
+        legend=dict(orientation="h", yanchor="bottom", y=1.02))
 
     return fig_demo, title
 
 
-demo_card = dbc.Card([
-    html.H4("Overdose Death Rate based on Demographic", id="demo_title"),
-    html.H6("Subtitle", id="demo_subtitle"),
-    dcc.Graph(id='demo_graph', figure=fig_demo)
-], body=True)
+demo_card = dbc.Card(
+    [html.H4("Overdose Death Rate based on Demographic", id="demo_title"), html.H6("Subtitle", id="demo_subtitle"),
+        dcc.Graph(id='demo_graph', figure=fig_demo)], body=True)
