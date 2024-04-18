97754aaa1f51e1607c656ba7bcc5a557fe7f7b15
SoloSynth1 <solosynth1@gmail.com>
2024-04-18 11:21:11 -0700

    reformat `demo.py`

--- src/modules/components/demo.py
+++ src/modules/components/demo.py
@@ -5,17 +5,17 @@ import dash_bootstrap_components as dbc
 
 from ..datasets import demo_df
 
-
 # create graph for the demographic
 fig_demo = go.Figure()
 
 
-@callback(
-    [Output('demo_graph', 'figure'),
-     Output('demo_subtitle', 'children')],
-    [Input('drug_type_list', 'value'),
-     Input('year_range_slider', 'value')]
-)
+@callback([
+    Output('demo_graph', 'figure'),
+    Output('demo_subtitle', 'children')
+], [
+    Input('drug_type_list', 'value'),
+    Input('year_range_slider', 'value')
+])
 def update_demo_figure(selected_drug, selected_years):
     start_year, end_year = selected_years
     filtered_df = demo_df[(demo_df['Year'].between(start_year, end_year))]
@@ -29,27 +29,22 @@ def update_demo_figure(selected_drug, selected_years):
     else:
         return go.Figure(), "Please select a drug type"
 
-    fig_demo = px.line(
-        filtered_df, x="Year", y="Death Rate", color="Demographic",
-        line_group="Demographic", color_discrete_sequence=px.colors.qualitative.T10)
+    fig_demo = px.line(filtered_df, x="Year", y="Death Rate", color="Demographic", line_group="Demographic",
+        color_discrete_sequence=px.colors.qualitative.T10)
 
     for trace in fig_demo.data:
         if trace.name == 'Overall':
-            trace.line = dict(width=5) 
+            trace.line = dict(width=5)
         else:
             trace.opacity = 0.7
         trace.name = trace.name.replace(" (Non-Hispanic)", "")
 
-    fig_demo.update_layout(
-        xaxis_title="Year",
-        yaxis_title="Death Rate <br>(per 100,000 population)",
-        legend=dict(
-            orientation="h", yanchor="bottom", y=1.02))
+    fig_demo.update_layout(xaxis_title="Year", yaxis_title="Death Rate <br>(per 100,000 population)",
+        legend=dict(orientation="h", yanchor="bottom", y=1.02))
 
     return fig_demo, title
 
-demo_card = dbc.Card([
-    html.H4("Overdose Death Rate based on Demographic", id="demo_title"),
-    html.H6("Subtitle", id="demo_subtitle"),
-    dcc.Graph(id='demo_graph', figure=fig_demo)
-], body=True)
+
+demo_card = dbc.Card(
+    [html.H4("Overdose Death Rate based on Demographic", id="demo_title"), html.H6("Subtitle", id="demo_subtitle"),
+        dcc.Graph(id='demo_graph', figure=fig_demo)], body=True)
