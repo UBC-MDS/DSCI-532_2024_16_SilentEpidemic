78bc2cd6535c0ce1fe1917fa3420176658488cb1
jinyz8888 <108684429+jinyz8888@users.noreply.github.com>
2024-04-19 21:09:15 -0700

    fix: incorrect average death rate (#141)
    * fix average death rate
    
    * fix average death rate formula
    - remove unused line
    - remove the percentage behind death rate
    - change death rate title to hint at normalization
    
    * change header to avoid ambiguity
    
    ---------
    
    Co-authored-by: SoloSynth1 <solosynth1@gmail.com>

--- src/modules/components/agg_vals.py
+++ src/modules/components/agg_vals.py
@@ -44,10 +44,10 @@ def update_aggregated_values(selected_sex, selected_years, selected_age):
     group_df = pop_df.groupby(['Year'])
     fold_change = group_df['Deaths'].sum().iloc[-1] / group_df['Deaths'].sum().iloc[0]
 
-    avg_death_rate = (pop_df['Death Rate'] * pop_df['Deaths']).sum() / pop_df['Deaths'].sum()
+    avg_death_rate = group_df['Death Rate'].mean().mean()
 
     formatted_deaths = f"{cumulative_deaths:,.0f}"
-    formatted_death_rate = f"{avg_death_rate:.2f}%"
+    formatted_death_rate = f"{avg_death_rate:.2f}"
     formatted_percentage_young_adults = f"{young_rate:.2f}%"
     fold_change_text = f"{fold_change:.1f}"
 
@@ -55,6 +55,6 @@ def update_aggregated_values(selected_sex, selected_years, selected_age):
 
 
 death_card = create_card("Cumulative Deaths from All Drugs", 0, "death_value")
-death_rate_card = create_card("Average Death Rate", 0, "death_rate_value")
+death_rate_card = create_card("Average Death Rate (All Drugs, per 100,000)", 0, "death_rate_value")
 percentage_card = create_card("Percentage Young Adult Deaths", 0, "percentage_value")
 fold_change_card = create_card("Fold Change", 0, "fold_change_value")
