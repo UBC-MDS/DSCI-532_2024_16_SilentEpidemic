efc4bb283c4dc6b63075f60646ed8d9a3138708c
SoloSynth1 <solosynth1@gmail.com>
2024-04-20 02:59:49 -0700

    create a pre-filtered dataframe for opioid chart

--- src/modules/datasets.py
+++ src/modules/datasets.py
@@ -4,3 +4,8 @@ import pandas as pd
 overall_df = pd.read_parquet('data/processed/overall.parquet')
 specific_df = pd.read_parquet('data/processed/specific.parquet')
 demo_df = pd.read_parquet('data/processed/demo.parquet')
+
+opioid_df = specific_df[
+    (specific_df["Drug Type"].isin(['Stimulants', 'Cocaine', 'Psychostimulants', 'Benzodiazepines', 'Antidepressants'])) &
+    (specific_df['Opioid Type'] == 'any')
+]
